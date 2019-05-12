8ce5c4181f073c512faf3e2315f5c4961ee1cbe3
Andy Bryant <a.mcdermid.bryant@gmail.com>
2019-05-12 16:34:55 -0400

    updated loading

--- js/app.js
+++ js/app.js
@@ -12,14 +12,9 @@ let timer = setInterval(function() {
   let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
   // display
-  document.getElementById("timer").innerHTML = "<div class=\"days\"> \
-  <div class=\"numbers\">" + days + "</div>days</div> \
-    <div class=\"hours\"> \
-    <div class=\"numbers\">" + hours + "</div>hours</div> \
-    <div class=\"minutes\"> \
-    <div class=\"numbers\">" + minutes + "</div>minutes</div> \
-    <div class=\"seconds\"> \
-    <div class=\"numbers\">" + seconds + "</div>seconds</div> \
-    </div>";
-
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+  
 }, 1000);
